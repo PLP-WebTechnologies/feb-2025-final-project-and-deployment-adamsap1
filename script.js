5f7b53f256b486a62dba3c019084f7006a650728
adamsap1 <adamsapeh35@gmail.com>
2025-05-12 09:29:40 +0100

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showPost(postElement) {
+    const content = postElement.querySelector('.full-content');
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  }
+  
